3d76416a5870550231d76789e8d6d8fd557ab98f
Daryln Too <darlyngtoo@gmail.com>
2020-05-16 12:45:51 +0800

    Fixed indentation errors in loan_processing page

--- views.py
+++ views.py
@@ -116,29 +116,29 @@ def loan_page():
 @application.route('/loans_processing', methods = ['POST', 'GET'])
 def loanprocessing_page():
     if request.method == 'POST':
-    result = request.form
+        result = request.form
 
-    firstName = result["firstName"]
-    lastName = result["lastName"]
-    preferredLanguage = "ENGLISH"
-    notes = result["grant"]
-    assignedBranchKey = "8a8e878e71c7a4d70171ca644def1259"
-    basicInfo = {"firstName": firstName, "lastName": lastName, "preferredLanguage": preferredLanguage, "notes": notes, "assignedBranchKey": assignedBranchKey}
+        firstName = result["firstName"]
+        lastName = result["lastName"]
+        preferredLanguage = "ENGLISH"
+        notes = result["grant"]
+        assignedBranchKey = "8a8e878e71c7a4d70171ca644def1259"
+        basicInfo = {"firstName": firstName, "lastName": lastName, "preferredLanguage": preferredLanguage, "notes": notes, "assignedBranchKey": assignedBranchKey}
 
-    identificationDocumentTemplateKey = "8a8e867271bd280c0171bf7e4ec71b01"
-    issuingAuthority = "Immigration Authority of Singapore"
-    documentType = "NRIC/Passport Number"
-    validUntil = "2021-09-12"
-    documentId = "S9812345A"
-    identity = [{"identificationDocumentTemplateKey":identificationDocumentTemplateKey, "issuingAuthority":issuingAuthority, "documentType":documentType, "validUntil":validUntil, "documentId":documentId}]
+        identificationDocumentTemplateKey = "8a8e867271bd280c0171bf7e4ec71b01"
+        issuingAuthority = "Immigration Authority of Singapore"
+        documentType = "NRIC/Passport Number"
+        validUntil = "2021-09-12"
+        documentId = "S9812345A"
+        identity = [{"identificationDocumentTemplateKey":identificationDocumentTemplateKey, "issuingAuthority":issuingAuthority, "documentType":documentType, "validUntil":validUntil, "documentId":documentId}]
 
-    createClientJson = json.dumps({"client":basicInfo, "idDocuments":identity})
-    print(createClientJson) # create client
+        createClientJson = json.dumps({"client":basicInfo, "idDocuments":identity})
+        print(createClientJson) # create client
 
 
-    headers = {'content-type': 'application/json'}
-    response = requests.post("https://razerhackathon.sandbox.mambu.com/api/clients", data=createClientJson, headers=headers, auth=('Team66', 'passEE8295411'))
-    print(response.text)
+        headers = {'content-type': 'application/json'}
+        response = requests.post("https://razerhackathon.sandbox.mambu.com/api/clients", data=createClientJson, headers=headers, auth=('Team66', 'passEE8295411'))
+        print(response.text)
 
 @application.route('/crowdsourcing')
 def crowdsourcing():
