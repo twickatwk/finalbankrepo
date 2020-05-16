d9ec49964528ae15c2c0d927bde7f9d0fec5cc8a
Daryln Too <darlyngtoo@gmail.com>
2020-05-16 12:36:55 +0800

    Added DB connection, login and logout functions
    - Updated requirements.txt
    - Added models.py (database tables)
    - Organized app (views.py for all the routes, app.py for main, models.py for tables)

--- application.py
+++ application.py
@@ -1,55 +1,33 @@
-from flask import Flask, redirect, url_for, render_template, request
-import json
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+from flask_login import LoginManager
+
 
 application = Flask(__name__)
 
-@application.route('/')
-def home_page():
-    return render_template("index.html")
+# Secret key for csrf
+SECRET_KEY = os.urandom(32)
+application.config['SECRET_KEY'] = SECRET_KEY
 
-@application.route('/grants')
-def grant_page():
-    return render_template('grants.html')
+#Database
+application.config.from_pyfile('config.cfg')
+db = SQLAlchemy(application)
 
-@application.route('/loans')
-def loan_page():
-    return render_template('loans.html')
+login_manager = LoginManager()
+login_manager.init_app(application)
+login_manager.session_protection = "strong"
+login_manager.login_view = 'index'
 
-@application.route('/loans_processing', methods = ['POST', 'GET'])
-def loanprocessing_page():
- if request.method == 'POST':
-  result = request.form
+@login_manager.user_loader
+def load_user(user_id):
+    from models import User
+    try:
+        return User.query.filter_by(user_id=user_id).first()
+    except:
+        return None
 
-  firstName = result["firstName"]
-  lastName = result["lastName"]
-  preferredLanguage = "ENGLISH"
-  notes = result["grant"]
-  assignedBranchKey = "8a8e878e71c7a4d70171ca644def1259"
-  basicInfo = {"firstName": firstName, "lastName": lastName, "preferredLanguage": preferredLanguage, "notes": notes, "assignedBranchKey": assignedBranchKey}
-
-  identificationDocumentTemplateKey = "8a8e867271bd280c0171bf7e4ec71b01"
-  issuingAuthority = "Immigration Authority of Singapore"
-  documentType = "NRIC/Passport Number"
-  validUntil = "2021-09-12"
-  documentId = "S9812345A"
-  identity = [{"identificationDocumentTemplateKey":identificationDocumentTemplateKey, "issuingAuthority":issuingAuthority, "documentType":documentType, "validUntil":validUntil, "documentId":documentId}]
-
-  createClientJson = json.dumps({"client":basicInfo, "idDocuments":identity})
-  print(createClientJson)
-
-  return render_template('loans_processing.html', result = result)
- else:
-  return redirect(url_for('loan_page'))
-
-@application.route('/crowdsourcing')
-def crowdsourcing():
-    return render_template('crowdsourcing.html')
-
-@application.route('/index2')
-def home_page2():
-    return render_template("index2.html")
-
-
+from views import *
 
 if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
---
+++ models.py
@@ -0,0 +1,37 @@
+from application import db
+from flask_login import UserMixin
+
+
+class User(db.Model, UserMixin):
+    user_id = db.Column(db.Integer, nullable=False, unique=True, primary_key=True)
+    user_name = db.Column(db.String(255), nullable=False, unique=True)
+    first_name = db.Column(db.String(255), unique=False)
+    last_name = db.Column(db.String(255), unique=False)
+    password = db.Column(db.String(100))
+    authenticated = db.Column(db.Boolean, default=False)
+
+    # one-to-many relationship
+    grants = db.relationship('Grant', backref='user', lazy=True)
+
+    def get_id(self):
+        return (self.user_id)
+
+    def is_authenticated(self):
+        """Return True if the user is authenticated."""
+        return self.authenticated
+    
+    def is_active(self):
+        """True, as all users are active."""
+        return True
+    
+    def is_anonymous(self):
+        """False, as anonymous users aren't supported."""
+        return False
+
+
+class Grant(db.Model):
+    grant_id = db.Column(db.Integer, primary_key=True, nullable=False, unique=True)
+    grant_name = db.Column(db.String(255), unique=False)
+    grant_amount = db.Column(db.Float)
+    user_name = db.Column(db.String(255), db.ForeignKey('user.user_name'), nullable=False)
+    grant_type = db.Column(db.String(255))
